695fb532e6931e526772cda7b62c103f33c1d1ef
Ayush Raj <ayush.raj138@gmail.com>
2023-07-13 22:14:32 +0530

    feat: added search functionality

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,7 +2,13 @@ import { NavLink } from "react-router-dom";
 
 import logo from "../../asssets/logo.png";
 import navbarCSS from "./navbar.module.css";
+import { useData } from "../../context/dataContext";
 export const Navbar = () => {
+  const {dispatch}=useData();
+  const changeHandler=(e)=>{
+    console.log(e.target.value)
+    dispatch({type:"SEARCH",payload:e.target.value})
+  }
   return (
     <nav className={navbarCSS.navbar}>
       <NavLink to="/">
@@ -10,7 +16,7 @@ export const Navbar = () => {
       </NavLink>
       <div>
         <span className="material-symbols-outlined">search</span>
-        <input type="text" placeholder="Search by title and t..." />'
+        <input type="text" placeholder="Search by title and t..." onChange={changeHandler}/>'
       </div>
     </nav>
   );
--- src/utils/dataReducer.js
+++ src/utils/dataReducer.js
@@ -1,18 +1,39 @@
 import { data } from "../db/data";
 
-export const initialState = { 
-    data:data.meetups,
-    filteredData:[...data.meetups],
-    filterBy:""
+export const initialState = {
+  data: data.meetups,
+  filteredData: [...data.meetups],
+  filterBy: "",
+  searchBy: "",
 };
 
 export const dataReducer = (state, action) => {
   const { type, payload } = action;
   switch (type) {
     case "EVENT_TYPE":
-        return {...state,filteredData:state?.data?.filter(({eventType})=>payload==="Both"?state?.data:eventType===payload)}
+      return {
+        ...state,
+        filteredData: state?.data?.filter(({ eventType }) =>
+          payload === "Both" ? state?.data : eventType === payload
+        ),
+      };
     case "PAYMENT":
-      return{...state,data:state?.data?.map((event)=>event.id===payload?{...event,isPaid:true}:event)}
+      return {
+        ...state,
+        data: state?.data?.map((event) =>
+          event.id === payload ? { ...event, isPaid: true } : event
+        ),
+      };
+    case "SEARCH":
+      console.log(state.data);
+      console.log(payload)
+      return {
+        ...state,
+        filteredData:
+          state?.data?.filter(({ title, eventTags }) =>
+            title.toLowerCase().includes(payload.toLowerCase())
+           || eventTags.includes(payload.toLowerCase()))
+      };
     default:
       return state;
   }
